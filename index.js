d8ff00fdaecbeebf74634cde29c4d060dbb08218
Gera-Bukhman <georgybukhman@gmail.com>
2025-05-23 17:09:09 +0500

    добавил eslint

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ console.log("Сколько вам лет?");
 const age = 18; // Здесь позже добавим ввод от пользователя
 const birthYear = getBirthYear(age);
 console.log(`Ваш год рождения: ${birthYear}`);
+
+let a = 5
+a++
